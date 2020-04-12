413133f28c1d2b2d7088612f89774bf71fa8e013
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-12 13:02:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function saturdayFun(activity = "roller-skate"){
 function mondayWork(activity = "go to the office"){
   return `This Monday, I will ${activity}.`
 }
+
+function wrapAdjective(flair = "*"){
+  return function(wish = 'special'){
+    return `You are ${flair}${wish}${flair}!`
+  }
+}
